dc6198e628043aa2f47d2821eb0bb320ac745f46
Scarlett <gourleys@tcd.ie>
2023-12-03 22:36:45 +0000

    Part 2 day 2

--- 2/main.c
+++ 2/main.c
@@ -16,42 +16,32 @@ const char colour_mapping[] = {
 	'b'
 };
 
-const int colour_total[] = {
-	12,
-	13,
-	14
-};
-
 // Return true if the number of blocks exceeds the total for that colour
-uint check_blocks(uint number, char token, enum Colour colour) {
-	if ((token == colour_mapping[colour]) && (number > colour_total[colour])) {
+uint check_blocks(uint number, char token, enum Colour colour, uint *colour_totals) {
+	if ((token == colour_mapping[colour]) && (number > colour_totals[colour])) {
 		return 1;
 	}
 	return 0;
 }
 
 int main() {
-	uint total_red = 12;
-	uint total_green = 13;
-	uint total_blue = 14;
-
-	uint total_possible = 0;
+	uint total_cube_power = 0;
 
 	for (int i = 0; i < sizeof(data)/sizeof(data[0]); i++) {
-		printf("\n%s\n", data[i]);
 		uint line_len = strlen(data[i]);
-		int possible = 1;
+		uint colour_minimum[] = {0, 0, 0};
 
-		// Format is "<number> <colour>," or "<number> <colour;"
+		// Format is "<number> <colour>, " or "<number> <colour>; " so just
+		// strstr to the next space until there is no more data
 		for(char *line_data = strstr(data[i], ":")+1;
-			line_data != NULL && possible;
+			line_data != NULL;
 			line_data = strstr(line_data, " ")) {
 
 			int j = 0;
 			int number_blocks = 0;
 			line_data++;
 
-			//printf("%s\n", line_data);
+			// Parse the number
 			while ('0' <= line_data[j] && line_data[j] <= '9') {
 				number_blocks = (number_blocks * 10) + (line_data[j]-'0');
 				j++;
@@ -59,29 +49,23 @@ int main() {
 			// Skip over the space
 			j++;
 
-			if (check_blocks(number_blocks, line_data[j], RED)) {
-				possible = 0;
-				printf("Number blocks to make this impossible for RED: %d\n", number_blocks);
+			if (check_blocks(number_blocks, line_data[j], RED, colour_minimum)) {
+				colour_minimum[RED] = number_blocks;
 			}
-			else if (check_blocks(number_blocks, line_data[j], GREEN)) {
-				printf("Number blocks to make this impossible for GREEN: %d\n", number_blocks);
-				possible = 0;
+			if (check_blocks(number_blocks, line_data[j], GREEN, colour_minimum)) {
+				colour_minimum[GREEN] = number_blocks;
 			}
-			else if (check_blocks(number_blocks, line_data[j], BLUE)) {
-				printf("Number blocks to make this impossible for BLUE: %d\n", number_blocks);
-				possible = 0;
+			if (check_blocks(number_blocks, line_data[j], BLUE, colour_minimum)) {
+				colour_minimum[BLUE] = number_blocks;
 			}
 
 			line_data += j;
 		}
 
-		if (possible) {
-			printf("Adding game %d\n", i+1);
-			total_possible += i+1;
-		}
+		total_cube_power += (colour_minimum[RED]*colour_minimum[GREEN]*colour_minimum[BLUE]);
 	}
 
-	printf("Total: %d\n", total_possible);
+	printf("Total: %d\n", total_cube_power);
 
 	return 0;
 }
