ff4b124dd651f78c91753be02a546b81bebc21c6
Scarlett <gourleys@tcd.ie>
2023-12-13 21:29:39 +0000

    Arrangement checking

--- 12/main.c
+++ 12/main.c
@@ -35,15 +35,76 @@ void free_list(struct Node *head) {
 enum Status {
 	OPERATIONAL = '.',
 	DAMAGED     = '#',
-	UNKOWN      = '?'
+	UNKNOWN      = '?'
 };
+
+uint consecutively_damaged(char *statuses, uint damaged_amount) {
+	uint consecutive = 0;
+	for (int i = 0; statuses[i] && i < damaged_amount; i++) {
+		if (statuses[i] != OPERATIONAL) {
+			consecutive++;
+		}
+	}
+	if (consecutive == damaged_amount) {
+		return 1;
+	}
+	return 0;
+}
+
+uint matching_arrangements(char *statuses, struct Node *arrangements) {
+	int i;
+	for (i = 0; statuses[i] && arrangements; i++) {
+		if (statuses[i] != DAMAGED) {
+			continue;
+		}
+		if (consecutively_damaged(statuses+i, arrangements->value)) {
+			printf("Position %d is damaged %u times\n", i, arrangements->value);
+			i += arrangements->value;
+			arrangements = arrangements->next;
+			if (statuses[i] == DAMAGED) {
+				printf("Match not found -- next is damaged\n");
+				return 0;
+			}
+		} else {
+			printf("Match not found -- next consecutive damage is not %u\n", arrangements->value);
+			return 0;
+		}
+	}
+	// Check we've used all the nodes, and there are no mode possible matches
+	if (!arrangements && (!statuses[i] || !consecutively_damaged(statuses+i, 1))) {
+		printf("Match found\n");
+		return 1;
+	}
+	printf("Match not found -- arrangements left\n");
+	return 0;
+}
+
+void populate_unknowns(char *statuses, uint start) {
+	uint unknowns;
+	for (int i = start; statuses[i]; i++) {
+		if (statuses[i] == UNKNOWN) {
+			unknowns++;
+
+			statuses[i] = DAMAGED;
+			populate_unknowns(statuses, i);
+
+			statuses[i] = OPERATIONAL;
+			populate_unknowns(statuses, i);
+
+			statuses[i] = UNKNOWN;
+		}
+	}
+	if (!unknowns) {
+		printf("%s\n", statuses);
+	}
+}
 
 // Main
 int main() {
-	const char **data  = test_input;
-	uint height = sizeof(test_input)/sizeof(data[0]);
+	const char **data  = bad_input;
+	uint height = sizeof(bad_input)/sizeof(data[0]);
 
-	enum Status **statuses = malloc(height * sizeof(enum Status*));
+	char **statuses = malloc(height * sizeof(char*));
 	struct Node **arrangements = malloc(height * sizeof(struct Node*));
 	for (int i = 0; i < height; i++) {
 		uint width  = strlen(data[i]);
@@ -51,7 +112,7 @@ int main() {
 		char *arrangements_data = strstr(line_data, " ")+1;
 		uint number_springs = arrangements_data - line_data;
 
-		statuses[i] = malloc(number_springs * sizeof(enum Status));
+		statuses[i] = malloc(number_springs * sizeof(char));
 
 		for (int j = 0; j < number_springs; j++) {
 			statuses[i][j] = line_data[j];
@@ -79,6 +140,11 @@ int main() {
 			printf("%u ", a->value);
 		}
 		printf("\n");
+		populate_unknowns(statuses[i], 0);
+
+		matching_arrangements(statuses[i], arrangements[i]);
+
+		printf("\n\n");
 	}
 
 	return 0;
